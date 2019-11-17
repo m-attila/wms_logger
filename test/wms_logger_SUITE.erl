1a1c22cf8fb7874e6f7bb0ba52b5bb86690c7267
Makra Attila <makraat@ext.otpbank.hu>
2019-11-17 04:58:21 +0100

    Error code could be set to meta

---
+++ test/wms_logger_SUITE.erl
@@ -0,0 +1,245 @@
+%%%-------------------------------------------------------------------
+%%% @author Attila Makra
+%%% @copyright (C) 2019, OTP Bank Nyrt.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 17. Nov 2019 04:42
+%%%-------------------------------------------------------------------
+-module(wms_logger_SUITE).
+-author("Attila Makra").
+
+-define(APP_NAME, wms_logger).
+
+-compile(nowarn_export_all).
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
+-include("wms_logger.hrl").
+
+%%--------------------------------------------------------------------
+%% COMMON TEST CALLBACK FUNCTIONS
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Function: suite() -> Info
+%%
+%% Info = [tuple()]
+%%   List of key/value pairs.
+%%
+%% Description: Returns list of tuples to set default properties
+%%              for the suite.
+%%
+%% Note: The suite/0 function is only meant to be used to return
+%% default data values, not perform any other operations.
+%%--------------------------------------------------------------------
+suite() ->
+  [{key, value}].
+
+%%--------------------------------------------------------------------
+%% Function: init_per_suite(Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the suite.
+%%
+%% Description: Initialization before the suite.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+  [{test_dir, code:lib_dir(?APP_NAME) ++ "/test"} | Config].
+
+%%--------------------------------------------------------------------
+%% Function: end_per_suite(Config0) -> term() | {save_config,Config1}
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% Description: Cleanup after the suite.
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% Function: init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%%
+%% GroupName = atom()
+%%   Name of the test case group that is about to run.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding configuration data for the group.
+%% Reason = term()
+%%   The reason for skipping all test cases and subgroups in the group.
+%%
+%% Description: Initialization before each test case group.
+%%--------------------------------------------------------------------
+init_per_group(GroupName, Config) ->
+  ?MODULE:GroupName({prelude, Config}).
+
+%%--------------------------------------------------------------------
+%% Function: end_per_group(GroupName, Config0) ->
+%%               term() | {save_config,Config1}
+%%
+%% GroupName = atom()
+%%   Name of the test case group that is finished.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding configuration data for the group.
+%%
+%% Description: Cleanup after each test case group.
+%%--------------------------------------------------------------------
+end_per_group(GroupName, Config) ->
+  ?MODULE:GroupName({postlude, Config}).
+
+%%--------------------------------------------------------------------
+%% Function: init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%%
+%% TestCase = atom()
+%%   Name of the test case that is about to run.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%%
+%% Description: Initialization before each test case.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%--------------------------------------------------------------------
+init_per_testcase(TestCase, Config) ->
+  ?MODULE:TestCase({prelude, Config}).
+
+%%--------------------------------------------------------------------
+%% Function: end_per_testcase(TestCase, Config0) ->
+%%               term() | {save_config,Config1} | {fail,Reason}
+%%
+%% TestCase = atom()
+%%   Name of the test case that is finished.
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for failing the test case.
+%%
+%% Description: Cleanup after each test case.
+%%--------------------------------------------------------------------
+end_per_testcase(TestCase, Config) ->
+  ?MODULE:TestCase({postlude, Config}).
+
+%%--------------------------------------------------------------------
+%% Function: groups() -> [Group]
+%%
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%%   The name of the group.
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%%   Group properties that may be combined.
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%%   The name of a test case.
+%% Shuffle = shuffle | {shuffle,Seed}
+%%   To get cases executed in random order.
+%% Seed = {integer(),integer(),integer()}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%%   To get execution of cases repeated.
+%% N = integer() | forever
+%%
+%% Description: Returns a list of test case group definitions.
+%%--------------------------------------------------------------------
+groups() ->
+  [
+    {logging_group,
+     [{repeat_until_any_fail, 1}],
+     [
+       logger_test
+     ]
+    }
+  ].
+
+%%--------------------------------------------------------------------
+%% Function: all() -> GroupsAndTestCases | {skip,Reason}
+%%
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%%   Name of a test case group.
+%% TestCase = atom()
+%%   Name of a test case.
+%% Reason = term()
+%%   The reason for skipping all groups and test cases.
+%%
+%% Description: Returns the list of groups and test cases that
+%%              are to be executed.
+%%--------------------------------------------------------------------
+all() ->
+  [
+    {group, logging_group}
+  ].
+
+%%--------------------------------------------------------------------
+%% TEST CASES
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% Function: TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%% Comment = term()
+%%   A comment about the test case that will be printed in the html log.
+%%
+%% Description: Test case function. (The name of it must be specified in
+%%              the all/0 list or in a test case group for the test case
+%%              to be executed).
+%%--------------------------------------------------------------------
+
+%% =============================================================================
+%% Start-stop group
+%% =============================================================================
+
+logging_group({prelude, Config}) ->
+  {ok, _} = application:ensure_all_started(?APP_NAME),
+  Config;
+logging_group({postlude, _}) ->
+  ok = application:stop(?APP_NAME),
+  ok.
+
+%%--------------------------------------------------------------------
+%% Logging function
+%%
+%%--------------------------------------------------------------------
+
+%% test case information
+logger_test({info, _Config}) ->
+  [""];
+logger_test(suite) ->
+  ok;
+%% init test case
+logger_test({prelude, Config}) ->
+  Config;
+%% destroy test case
+logger_test({postlude, _Config}) ->
+  ok;
+%% test case implementation
+logger_test(_Config) ->
+  ?debug("Simple debug"),
+  ?debug("Debug with param: ~s", [param1]),
+
+  ?info("Simple info"),
+  ?info("Info with param: ~s", [param1]),
+
+  ?warning("Simple warning"),
+  ?warning("Warning with param: ~s", [param1]),
+
+  ?error("Simple error"),
+  ?error("Error with param: ~s", [param1]),
+  ?error("0001", "Error with param and code: ~s", [param1]).
+
